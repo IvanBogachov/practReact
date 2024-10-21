2a2bcb6a14a47bc5265691bb9e5d0fbeb1550a23
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-10-21 21:37:41 +0300

    add ....

---
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -0,0 +1,28 @@
+import { Button } from "../Button/Button";
+import { useSelector, useDispatch } from "react-redux";
+// 1. Імпортуємо хук
+// 2. Імпортуємо фабрику екшену
+import { setStatusFilter } from "../../redux/actions";
+import css from "./StatusFilter.module.css";
+
+export const StatusFilter = () => {
+  // 3. Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+  const filter = useSelector((state) => state.filters.status);
+  // 4. Викликаємо фабрику екшену та передаємо значення фільтра
+  // 5. Відправляємо результат - екшен зміни фільтра
+  const handleFilterChange = (filter) => dispatch(setStatusFilter(filter));
+  return (
+    <div className={css.wrapper}>
+      <Button onClick={() => handleFilterChange("all")}>
+        All {filter === "all" && "is active"}
+      </Button>
+      <Button onClick={() => handleFilterChange("active")}>
+        Active {filter === "active" && "is active"}
+      </Button>
+      <Button onClick={() => handleFilterChange("completed")}>
+        Completed {filter === "completed" && "is active"}
+      </Button>
+    </div>
+  );
+};
---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,35 @@
+import { MdClose } from "react-icons/md";
+// 1. Імпортуємо хук
+import { useDispatch } from "react-redux";
+// 2. Імпортуємо фабрику екшену
+import { deleteTask, toggleCompleted } from "../../redux/actions";
+import css from "./Task.module.css";
+
+export const Task = ({ task }) => {
+  // 3. Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+  // 4. Викликаємо фабрику екшену та передаємо ідентифікатор завдання
+  // 5. Відправляємо результат - екшен видалення завдання
+  const handleDelete = () => {
+    dispatch(deleteTask(task.id));
+  };
+  // 4. Викликаємо фабрику екшену та передаємо ідентифікатор завдання
+  // 5. Відправляємо результат - екшен перемикання статусу завдання
+  const handleToggle = () => {
+    dispatch(toggleCompleted(task.id));
+  };
+  return (
+    <div className={css.wrapper}>
+      <input
+        type="checkbox"
+        className={css.checkbox}
+        onChange={handleToggle}
+        checked={task.completed}
+      />
+      <p className={css.text}>{task.text}</p>
+      <button className={css.btn} onClick={handleDelete}>
+        <MdClose size={24} />
+      </button>
+    </div>
+  );
+};
